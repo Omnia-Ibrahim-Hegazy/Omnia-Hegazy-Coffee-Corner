54fb8d9a738e9ed28fca459ce11737f3e8dd3261
Omnia-Ibrahim-Hegazy <omniahegazy929@gmail.com>
2024-12-14 23:54:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === 'flex') {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'flex';
+    }
+});
